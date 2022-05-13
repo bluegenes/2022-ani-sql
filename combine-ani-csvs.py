bffdda5584757d08c14ea86cc7d5e3e4dcce504f
N. Tessa Pierce <ntpierce@gmail.com>
2022-05-13 12:58:25 -0700

    read and combine linref ani csv

---
+++ combine-ani-csvs.py
@@ -0,0 +1,68 @@
+
+# build LCA ANI table from linref csv(s)
+import sys
+import argparse
+import sqlite3
+import csv
+import numpy as np
+from sourmash.tax import tax_utils
+from sourmash.lca import lca_utils
+from sourmash.logging import notify
+
+
+def main(args):
+
+    # first read reference -->
+    comparisonD = {}
+    with open(args.ref_ani_csv, 'r') as pf:
+        ref_r = csv.DictReader(pf)
+        for n, row in enumerate(ref_r):
+            comp_name = row["comparison"]
+            row["ref_ani"] = row["ani"]
+            del row['ani']
+            # store in dict
+            comparisonD[comp_name] = row
+
+    # now read in sourmash ANI and match
+    with open(args.sourmash_ani_csv, 'r') as pf:
+        ani_r = csv.DictReader(pf)
+        for n, row in enumerate(ani_r):
+            if n % 10000 == 0:
+                notify(f"row {n}")
+            comp_name = row["comparison"]
+            query_name= row["query_name"]
+            query = tax_utils.get_ident(query_name)
+            match_name = row["match_name"]
+            match = tax_utils.get_ident(match_name)
+            rev_comp_name = f"{match}_{query}"
+
+            fmh_ani = row["ani"]
+            # get dict entry and store
+            ref_entry = comparisonD.get(comp_name)
+            if ref_entry:
+                ref_entry["fmh_ani"] = fmh_ani
+                comparisonD[comp_name] = ref_entry
+            else:
+                ref_entry = comparisonD.get(rev_comp_name)
+                if ref_entry:
+                    ref_entry["fmh_ani"] = fmh_ani
+                    comparisonD[rev_comp_name] = ref_entry
+
+
+    with open(args.output_csv, 'w') as outF:
+        fields = ["comparison", "query_name", "match_name", "lca_rank", "lca_lineage", "ref_ani", "fmh_ani"]
+        writer = csv.DictWriter(outF, fieldnames=fields)
+        writer.writeheader()
+
+        for comp, compinfo in comparisonD.items():
+            if "fmh_ani" in compinfo.keys():
+                writer.writerow(compinfo)
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument('--ref-ani-csv', required=True, help = "LCA csv of reference ANI comparisons")
+    p.add_argument('--sourmash-ani-csv', required=True, help= "LCA csv of sourmash comparisons")
+    p.add_argument('-o', '--output-csv', required=True, help='output csv')
+    args = p.parse_args()
+    sys.exit(main(args))
--- linref-to-lca-csv.py
+++ linref-to-lca-csv.py
@@ -1,5 +1,5 @@
 
-# build LCA ANI table from prefetch csv(s)
+# build LCA ANI table from linref csv(s)
 import sys
 import argparse
 import sqlite3
@@ -44,53 +44,75 @@ def main(args):
     # load in taxonomy
     tax_assign = tax_utils.MultiLineageDB.load(args.taxonomy_csvs, keep_identifier_versions=False)
 
+
     # handle file input
-    prefetch_csvs = args.prefetch_csvs
+    linref_csvs = args.linref_csvs
     if args.from_file:
         ff_csvs = [x.strip() for x in open(args.from_file, 'r')]
-        prefetch_csvs += ff_csvs
+        linref_csvs += ff_csvs
 
+    gid_to_acc = {}
     # read in each file and load into table
     with open(args.output_csv, 'w') as outF:
-        fields = ["comparison", "query_id", "match_id", "lca_rank", "lca_lineage", "ani"]
+        fields = ["comparison", "query_name", "match_name", "lca_rank", "lca_lineage", "ani"]
         writer = csv.writer(outF)
         writer.writerow(fields)
 
         comparisons = set()
-        for inF in prefetch_csvs:
+        missing_ids = set()
+        n_missing_comparisons = 0
+        n_missing_lin = 0
+        for inF in linref_csvs:
             with open(inF, 'r') as pf:
-                prefetch_r = csv.DictReader(pf)
-                for n, row in enumerate(prefetch_r):
+                linref_r = csv.DictReader(pf)
+                for n, row in enumerate(linref_r):
                     if n % 10000 == 0:
                         notify(f"row {n+1}")
-                    query_name = row['query_name']
-                    query_id = tax_utils.get_ident(query_name)
-                    match_name = row['match_name']
-                    match_id = tax_utils.get_ident(match_name)
+                    query_id = row["Genome_ID"]
+                    query_acc = row["AssemblyID"]
 
-                    comparison_name = f"{query_id}_x_{match_id}"
-                    reverse_name = f"{match_id}_x_{query_id}"
+                    # drop version num
+                    query_acc = query_acc.rsplit('.')[0]
+                    # add query info to gid_to_acc
+                    gid_to_acc[query_id] = query_acc
+
+                    subject_id = row["SubjectGenome"]
+                    # get subj acc from dict (should work bc they're ordered -- should have subj id by the time it appears)
+                    try:
+                        subject_acc = gid_to_acc[subject_id]
+                    except KeyError:
+                        missing_ids.add(subject_id)
+                        n_missing_comparisons +=1
+                        continue # skip this entry
+
+                    comparison_name = f"{query_acc}_x_{subject_acc}"
+                    reverse_name = f"{subject_acc}_x_{query_acc}"
 
                     if any([comparison_name in comparisons, reverse_name in comparisons]):
                         # avoid dupes
                         continue
                     comparisons.add(comparison_name)
 
-                    lca_lin = get_lca(query_id, match_id, tax_assign)
+                    lca_lin = get_lca(query_acc, subject_acc, tax_assign)
                     if lca_lin is None:
                         # if missing lineage, can't get LCA. Skip.
+                        n_missing_lin +=1
                         continue
-                    ani = np.mean([float(row['query_ani']), float(row['match_ani'])])
+                    ani = row["ANI"]
 
                     # write csv
-                    writer.writerow([comparison_name, query_id, match_id, lca_lin[-1].rank, lca_lin[-1].name, ani])
+                    writer.writerow([comparison_name, query_acc, subject_acc, lca_lin[-1].rank, lca_lin[-1].name, ani])
+
+            print(f"missed {len(missing_ids)} ids, which resulted in {n_missing_comparisons} skipped comparisons")
+            print(f"could not find LCA for {n_missing_lin} comparisons")
+            print(", ". join(missing_ids))
 
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument('prefetch_csvs', nargs='*')
-    p.add_argument('--from-file', '--prefetch-from-file', help="file containing paths to prefetch csvs")
+    p.add_argument('linref_csvs', nargs='*')
+    p.add_argument('--from-file', '--linref-from-file', help="file containing paths to linref csvs")
     p.add_argument('-t', '--taxonomy-csvs', nargs='+', help='taxonomy information', required=True)
     p.add_argument('-o', '--output-csv', required=True, help='output csv')
     args = p.parse_args()
